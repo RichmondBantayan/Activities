2e15668d07a6a14ef120bf7af178e1622d0157ec
Richmond <RichmondBantayan.1st@gmail.com>
2025-09-06 16:03:03 +0800

    Add files via upload

---
+++ GameApp/Program.cs
@@ -0,0 +1,21 @@
+﻿using GameApp.Equipment;
+using GameApp.Libraries.Abstracts;
+
+namespace GameApp
+{
+    public class Program 
+    {
+        public static void Main(string[] args)
+        {
+            GameItem sld = new Shield("Iron Shield", 25);
+            GameItem pot = new Potion("Potion", 70);
+
+            sld.ShowInfo();
+            sld.UseItem();
+            pot.ShowInfo();
+            pot.UseItem();
+
+            ProgramBonus.Bonus();
+        }
+    }
+}
